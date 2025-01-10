9a5e5b1b6c6f3e7592b7c831aee2b7c3f5b923de
BiggerThings <nnsbiggerthings@gmail.com>
2025-01-10 03:50:12 +0200

    updateing directory page with directory component and category-item component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item.component.jsx";
+
+
+const Directory = ({ categories }) => {
+    return (
+        <div className="directory-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} title={category.title} imageUrl={category.imageUrl} />
+            ))}
+        </div>
+    )
+}
+export default Directory;
