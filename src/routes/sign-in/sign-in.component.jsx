4a74e41d8d3d7b73a99ff1aa77aedc6aa9994618
BiggerThings <nnsbiggerthings@gmail.com>
2025-01-20 17:00:20 +0200

    Created custom sign-up component and styled it

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
-import SignUpForm from "../../components/sign-up-form/sign-up.component";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
     const logGoogleUser = async () => {
