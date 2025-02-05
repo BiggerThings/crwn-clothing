3fd63aa16517dea9a56ad5b6a53eb703ad56f142
BiggerThings <nnsbiggerthings@gmail.com>
2025-02-05 18:09:47 +0200

    Reverting changes and fixing conflicts

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,33 +1,13 @@
 import { useState, createContext } from 'react';
 
-const addCartItem = (cartItems, productToAdd) => {
-    const existingCartItem = cartItems.find((item) => item.id === productToAdd.id);
-
-    if (existingCartItem) {
-        return cartItems.map((item) =>
-            item.id === productToAdd.id ? { ...item, quantity: item.quantity + 1 } : item
-        );
-    }
-
-    return [...cartItems, { ...productToAdd, quantity: 1 }];
-};
-
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
-    cartItems: [],
-    addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItem] = useState([]);
-
-    const addItemToCart = (productToAdd) => {
-        setCartItem(addCartItem(cartItems, productToAdd));
-    };
-
-    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+    const value = { isCartOpen, setIsCartOpen };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
