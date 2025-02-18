e43291e7ef4bcf0530761c69791b2ae601e64428
BiggerThings <nnsbiggerthings@gmail.com>
2025-02-18 10:36:35 +0200

    Styles Components: Navigation Route

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,9 +5,8 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
-import "./navigation.styles.scss"
-import { signOutUser } from  "../../utils/firebase/firebase.utils";;
-
+import { signOutUser } from  "../../utils/firebase/firebase.utils";
+import { NavigationContainer, LogoContainer, NavLinks, NavLink } from  "./navigation.styles.jsx";
 
 const Navigation = () => {
     const { currentUser  } = useContext(UserContext);
@@ -15,29 +14,29 @@ const Navigation = () => {
 
     return (
       <Fragment>
-        <div className="navigation">
-            <Link className="logo-container" to="/">
+        <NavigationContainer>
+            <LogoContainer to="/">
               <CrwnLogo className="logo" />
-            </Link>
-            <div className="nav-links-container">
-              <Link className="nav-link" to="/shop">
+            </LogoContainer>
+            <NavLinks>
+              <NavLink to="/shop">
                   SHOP
-              </Link>
+              </NavLink>
               {
                 currentUser ? 
-                <span className="nav-link" onClick={signOutUser}>
+                <NavLink as="span" onClick={signOutUser}>
                   SIGN-OUT
-                </span> :
-                <Link className="nav-link" to="/auth">
+                </NavLink> :
+                <NavLink to="/auth">
                   SIGN-IN
-                </Link>
+                </NavLink>
               }
               <CartIcon />
-            </div>
+            </NavLinks>
             {
               isCartOpen && <CartDropdown />
             }
-        </div>
+        </NavigationContainer>
         <Outlet />  
       </Fragment>
     );
