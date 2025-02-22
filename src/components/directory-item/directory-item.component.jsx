fa1a4341e299c08204f33dcadcbb7551856d50fd
BiggerThings <nnsbiggerthings@gmail.com>
2025-02-22 05:14:46 +0200

    Styled Components: cart-icon and directory-item

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,21 @@
-import "./directory-item.styles.scss";
+import {
+  DirectoryItemContainer,
+  BackgroundImage,
+  Body
+ } from "./directory-item.styles.jsx";
 
 const DirectoryItem = ({title, imageUrl}) => {
-    return (
-        <div className="directory-item-container">
-            <div className="background-image" style={{
-              backgroundImage: `url(${imageUrl})`
-            }}/>
-             <div className="body">
-               <h2>{title}</h2>
-               <p>Shop Now</p>
-             </div>
-        </div>
-    )
+  return (
+    <DirectoryItemContainer>
+        <BackgroundImage 
+          imageUrl={imageUrl}
+        />
+        <Body>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </Body>
+    </DirectoryItemContainer>
+  )
 }
 
 export default DirectoryItem;
