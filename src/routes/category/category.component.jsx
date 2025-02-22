f9ca2487c80fee12e8fbc2d472b69e7b82ed4db1
BiggerThings <nnsbiggerthings@gmail.com>
2025-02-22 04:37:21 +0200

    Styled component: authentication, category, checkout

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router";
 import { useContext, useState, useEffect, Fragment } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
-import "./category.styles.scss";
+import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
 
 const Category = () => {
     const { category } = useParams();
@@ -17,14 +17,14 @@ const Category = () => {
 
     return (
         <Fragment>
-            <h2 className="category-title">{category.toUpperCase()}</h2>
-            <div  className="category-container">
+            <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+            <CategoryContainer>
                 {products &&
                     products.map((product) => (
                         <ProductCard key={product.id} product={product} />
                     ))
                 }
-            </div>
+            </CategoryContainer>
         </Fragment>
     )
 }
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,7 +1,12 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-import "./checkout.styles.scss";
+import { 
+    CheckoutContainer,
+    CheckoutHeader,
+    HeaderBlock,
+    CheckoutTotal
+} from "./checkout.styles.jsx";
 
 const Checkout = () => {
     const { cartItems, setIsCartOpen, cartItemsTotal } = useContext(CartContext);
@@ -10,31 +15,31 @@ const Checkout = () => {
     
 
     return (
-        <div className="checkout-container">
-            <div className="checkout-header">
-                <div className="header-block">
+        <CheckoutContainer>
+            <CheckoutHeader>
+                <HeaderBlock>
                     <span>Product</span>
-                </div>
-                <div className="header-block">
+                </HeaderBlock>
+                <HeaderBlock>
                     <span>Description</span>
-                </div> 
-                <div className="header-block">
+                </HeaderBlock> 
+                <HeaderBlock>
                     <span>Quantity</span>
-                </div> 
-                <div className="header-block">
+                </HeaderBlock> 
+                <HeaderBlock>
                     <span>Price</span>
-                </div> 
-                <div className="header-block">
+                </HeaderBlock> 
+                <HeaderBlock>
                     <span>Remove</span>
-                </div>             
-            </div>
+                </HeaderBlock>             
+            </CheckoutHeader>
              {
                 cartItems.map((cartItem) => (
                     <CheckoutItem key={cartItem.id} cartItem={cartItem} />
                 ))
              }
-             <span className="total">Cart Total is ${cartItemsTotal}</span>
-        </div>
+             <CheckoutTotal>Cart Total is ${cartItemsTotal}</CheckoutTotal>
+        </CheckoutContainer>
     );
 };
 
