6caa1b74d95db46691c883986384e7bbd34f6ec4
BiggerThings <nnsbiggerthings@gmail.com>
2025-09-02 21:18:38 +0200

    Moved custom logger middleware from root reducer into its own file inside middleware folder

--- src/store/store.js
+++ src/store/store.js
@@ -2,22 +2,9 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
-//import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-
-    console.log('type', action.type);
-    console.log('payload', action.payload);
-    console.log('currentState', store.getState());
-
-    next(action);
-    console.log('next state', store.getState());
-};
 
 const persistConfig = {
     key: 'root',
@@ -27,7 +14,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
 
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
