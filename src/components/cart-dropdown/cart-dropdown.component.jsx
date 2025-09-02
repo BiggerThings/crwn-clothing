b2508da5906d664ea9a36878d7ed26e243deb03b
BiggerThings <nnsbiggerthings@gmail.com>
2025-09-02 07:21:43 +0200

    Migrated cart context: Product Card, Cart Dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,12 @@
 import { useNavigate } from "react-router";
-import { useContext } from "react";
-import { CartContext } from "../../contexts/cart.context";
+import { useSelector } from "react-redux";
+import { selectCartItems } from "../../store/cart/cart.selector";
 import { CartDropdownContainer, CartItems, EmptyMessage } from "./cart-dropdown.styles.jsx";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
-    const { cartItems } = useContext(CartContext);
+    const cartItems = useSelector(selectCartItems);
     const navigate = useNavigate();
 
     const goToCheckoutHandler = () => {
@@ -17,11 +17,11 @@ const CartDropdown = () => {
         <CartDropdownContainer>
             <CartItems>
                 {
-                    cartItems.length === 0 ? 
+                    cartItems.length === 0 ?
                         <EmptyMessage>Your cart is empty</EmptyMessage> :
-                    cartItems.map((cartItem) => (
-                        <CartItem key={cartItem.id} cartItem={cartItem} />
-                    ))
+                        cartItems.map((cartItem) => (
+                            <CartItem key={cartItem.id} cartItem={cartItem} />
+                        ))
                 }
             </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
