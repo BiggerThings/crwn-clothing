87f8d989c6b0f674b143eab584f9a9a84877e177
BiggerThings <nnsbiggerthings@gmail.com>
2025-09-04 21:16:36 +0200

    Redux Thunk: Added Spinner componenet to handle loading state of categories in category and category-preview componenets

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,18 +1,21 @@
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import { selectCategoriesMap, selectIsCategoriesLoading } from "../../store/categories/category.selector";
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
+import Spinner from "../../components/spinner/spinner.component.jsx";
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
-
+  const isLoading = useSelector(selectIsCategoriesLoading);
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        const category = categoriesMap[title];
-        return <CategoryPreview key={title} title={title} products={category} />
-      })}
-    </Fragment>
+    <>
+      {isLoading ? <Spinner /> : <Fragment>
+        {Object.keys(categoriesMap).map((title) => {
+          const category = categoriesMap[title];
+          return <CategoryPreview key={title} title={title} products={category} />
+        })}
+      </Fragment>}
+    </>
   );
 }
 
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,13 +1,15 @@
 import { useParams } from "react-router";
 import { useState, useEffect, Fragment } from "react";
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/category.selector.js";
+import { selectCategoriesMap, selectIsCategoriesLoading } from "../../store/categories/category.selector.js";
 import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component.jsx";
 import { CategoryContainer, CategoryTitle } from "./category.styles.jsx";
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectIsCategoriesLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
@@ -17,13 +19,13 @@ const Category = () => {
     return (
         <Fragment>
             <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-            <CategoryContainer>
+            {isLoading ? <Spinner /> : <CategoryContainer>
                 {products &&
                     products.map((product) => (
                         <ProductCard key={product.id} product={product} />
                     ))
                 }
-            </CategoryContainer>
+            </CategoryContainer>}
         </Fragment>
     )
 }
