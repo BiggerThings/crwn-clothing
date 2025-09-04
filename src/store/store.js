74773fde3de38e2aa3cee259ec1ae637b7618ee0
BiggerThings <nnsbiggerthings@gmail.com>
2025-09-04 07:09:33 +0200

    Redux Thunk: installed redux thunk library, set up reducer and actions to handle state of async effect i.e start, success or failure

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import logger from 'redux-logger';
+import { thunk } from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
@@ -14,7 +15,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
