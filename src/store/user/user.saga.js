c9a8a04877055de4531e92b8dd1a5648d4e41d50
BiggerThings <nnsbiggerthings@gmail.com>
2025-09-11 21:32:48 +0200

    Redux Saga: Checked user session on sign in (user saga)

---
+++ src/store/user/user.saga.js
@@ -0,0 +1,25 @@
+import { takeLatest, all, call, put } from "redux-saga/effects";
+
+import { USER_ACTION_TYPES } from "./user.types";
+
+import { signInSuccess, signInFailed } from "./user.action";
+
+import { getCurrentUser } from "../../utils/firebase/firebase.utils";
+
+export function* isUserAuthenticated() {
+    try {
+        const userAuth = yield call(getCurrentUser);
+        if (!userAuth) return;
+        yield put(signInSuccess(userAuth));
+    } catch (error) {
+        yield put(signInFailed(error));
+    }
+}
+
+export function* onCheckUserSession() {
+    yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function* userSagas() {
+    yield all([call(onCheckUserSession)]);
+}
