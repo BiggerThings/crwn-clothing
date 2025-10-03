4d40b92d928f697714acec90fdd3d8a3b8e0ec9b
BiggerThings <nnsbiggerthings@gmail.com>
2025-10-03 15:45:13 +0200

    Redux Saga: Sign Out Sagas

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,25 @@
 import { Outlet } from "react-router";
 import { Fragment } from "react";
-import { useSelector } from "react-redux";
+
+import { useSelector, useDispatch } from "react-redux";
+import { signOutStart } from "../../store/user/user.action";
+
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { selectIsCartOpen } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from "../../store/user/user.selector";
-import { signOutUser } from "../../utils/firebase/firebase.utils";
+
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles.jsx";
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
+
+  const signOutHandler = async () => {
+    dispatch(signOutStart());
+  };
 
   return (
     <Fragment>
@@ -25,7 +33,7 @@ const Navigation = () => {
           </NavLink>
           {
             currentUser ?
-              <NavLink as="span" onClick={signOutUser}>
+              <NavLink as="span" onClick={signOutHandler}>
                 SIGN-OUT
               </NavLink> :
               <NavLink to="/auth">
