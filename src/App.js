62edf48d2935d8681e4acec2eee25b7541e89cf2
BiggerThings <nnsbiggerthings@gmail.com>
2026-05-05 22:17:06 +0200

    Used React lazy + suspense to dynamically import routes from the home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 
-import Home from "./routes/home/home.component.jsx";
 import { Route, Routes } from "react-router";
-import Navigation from "./routes/navigation/navigation.component.jsx";
-import Authentication from "./routes/authentication/authentication.component.jsx";
-import Shop from "./routes/shop/shop.component.jsx";
-import Checkout from "./routes/checkout/checkout.component.jsx";
+
+import Spinner from "./components/spinner/spinner.component.jsx";
 
 import { checkUserSession } from "./store/user/user.action";
+
+const Home = lazy(() => import("./routes/home/home.component.jsx"));
+const Navigation = lazy(() => import("./routes/navigation/navigation.component.jsx"));
+const Authentication = lazy(() => import("./routes/authentication/authentication.component.jsx"));
+const Shop = lazy(() => import("./routes/shop/shop.component.jsx"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component.jsx"));
 
 
 const App = () => {
@@ -16,17 +19,19 @@ const App = () => {
 
   useEffect(() => {
     dispatch(checkUserSession());
-  }, []);
+  }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
