34afb56562f0bbbfb5faecd2c517129ad34bb1bd
BiggerThings <nnsbiggerthings@gmail.com>
2026-05-05 19:51:08 +0200

    Removed useMemo tutorial code

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useMemo } from "react";
+import { useCallback } from "react";
 
 import { useNavigate } from "react-router";
 import { useSelector } from "react-redux";
@@ -13,28 +13,9 @@ import {
     EmptyMessage
 } from "./cart-dropdown.styles.jsx";
 
-const sleep = (milliseconds) => {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds) {
-            break;
-        }
-    }
-}
-
 const CartDropdown = () => {
     const cartItems = useSelector(selectCartItems);
     const navigate = useNavigate();
-    const [count, setCount] = useState(0);
-
-    const hundredCount = () => {
-        console.log('start');
-        sleep(2000);
-        console.log('end');
-        return 100 + count;
-    }
-
-    const val = useMemo(() => hundredCount(), [count]);
 
     const goToCheckoutHandler = useCallback(() => {
         navigate("/checkout");
@@ -43,16 +24,15 @@ const CartDropdown = () => {
     return (
         <CartDropdownContainer>
             <CartItems>
-                {val}
-                {/* {
+                {
                     cartItems.length === 0 ?
                         <EmptyMessage>Your cart is empty</EmptyMessage> :
                         cartItems.map((cartItem) => (
                             <CartItem key={cartItem.id} cartItem={cartItem} />
                         ))
-                } */}
+                }
             </CartItems>
-            <Button onClick={() => setCount(count + 1)}>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
         </CartDropdownContainer>
     )
 }
