13eacc497953d64d735bb3181f6c562036512e2a
BiggerThings <nnsbiggerthings@gmail.com>
2026-06-11 22:47:02 +0200

    Added media query for category-preview and product card item mobile view height

--- src/components/category-preview/category-preview.styles.jsx
+++ src/components/category-preview/category-preview.styles.jsx
@@ -6,6 +6,10 @@ export const CategoryPreviewContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 30px;
+
+  @media screen and (max-width: 800px) {
+    align-items: center;
+  }
 `
 
 export const CategoryPreviewTitle = styled(Link)`
@@ -17,6 +21,16 @@ export const CategoryPreviewPreview = styled(Link)`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 15px;
+    grid-row-gap: 25px;
+  }
+
+  @media screen and (max-width: 400px) {
+    grid-template-columns: 1fr;
+    grid-column-gap: 55px;
+  }
 `
 
-  
--- src/components/product-card/product-card.styles.jsx
+++ src/components/product-card/product-card.styles.jsx
@@ -33,6 +33,34 @@ export const ProductCardContainer = styled.div`
       opacity: 0.8;
     }
   }
+
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+
+    button {
+      display: block;
+      opacity: 0.9;
+      min-width: unset;
+      padding: 0 10px;
+
+      %&:hover {
+        img {
+          opacity: unset;
+        }
+
+        button {
+          opacity: unset;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 400px) {
+    grid-template-columns: 1fr;
+    grid-column-gap: 55px;
+
+    width: 80vw;
+  }
 `
 
 export const ProductCardFooter = styled.div`
@@ -52,4 +80,3 @@ export const ProductCardPrice = styled.span`
   width: 10%;
 `
 
-  
