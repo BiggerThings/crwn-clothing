21f488d9dbaeb19c158df25b1713002e4a57c7df
BiggerThings <nnsbiggerthings@gmail.com>
2026-09-01 21:58:59 +0200

    RTL: Completed Button Component Tests

--- src/components/button/__tests__/button.component.test.js
+++ src/components/button/__tests__/button.component.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Button from '../button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button.component';
 
 describe('Button component', () => {
     it('render the base button by default', () => {
@@ -9,4 +9,24 @@ describe('Button component', () => {
         expect(buttonElement).toHaveStyle('background-color: white');
     })
 
+    it('render the google button when type is google', () => {
+        render(<Button buttonType={BUTTON_TYPE_CLASSES.google} />)
+
+        const googleButtonElement = screen.getByRole('button');
+        expect(googleButtonElement).toHaveStyle('color: white');
+    })
+
+    it('render the inverted button when type is inverted', () => {
+        render(<Button buttonType={BUTTON_TYPE_CLASSES.inverted} />)
+
+        const invertedButtonElement = screen.getByRole('button');
+        expect(invertedButtonElement).toHaveStyle('background-color: black');
+    })
+
+    it('should be disabled if isLoading is true', () => {
+        render(<Button isLoading={true} />);
+
+        const buttonElement = screen.getByRole('button');
+        expect(buttonElement).not.toBeDisabled();
+    })
 })    
