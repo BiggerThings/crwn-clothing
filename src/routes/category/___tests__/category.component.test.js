e3486e9d30facca1c7a2c86b5d4e359e31c98160
BiggerThings <nnsbiggerthings@gmail.com>
2026-09-25 07:17:10 +0200

    RTL: Category component when isLoading is false with products

--- src/routes/category/___tests__/category.component.test.js
+++ src/routes/category/___tests__/category.component.test.js
@@ -24,19 +24,39 @@ describe('Category component Tests', () => {
         expect(spinner).toBeInTheDocument();
     });
 
-    test('It should not render a spinner when isLoading is false', () => {
+    test('It should render Categories when isLoading is false', () => {
         renderWithProvider(<Category />, {
             preloadedState: {
                 categories: {
                     isLoading: false,
-                    categories: [],
+                    categories: [
+                        {
+                            title: 'mens',
+                            items: [
+                                {
+                                    id: 1,
+                                    name: 'Item 1',
+                                    price: 100
+                                },
+                                {
+                                    id: 2,
+                                    name: 'Item 2',
+                                    price: 150
+                                }
+                            ],
+                        }
+                    ],
                 },
             },
         });
 
         const spinner = screen.queryByTestId('spinner');
         expect(spinner).not.toBeInTheDocument();
+
+        const item1Product = screen.getByText('Item 1');
+        expect(item1Product).toBeInTheDocument();
+
+        const item2Product = screen.getByText('Item 2');
+        expect(item2Product).toBeInTheDocument();
     });
-
-
 })
