9f96e0bbe33140254a4768c439409059796f1c6e
BiggerThings <nnsbiggerthings@gmail.com>
2026-09-25 07:08:05 +0200

    RTL: Category component when isLoading is true

---
+++ src/routes/category/___tests__/category.component.test.js
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react';
+import Category from '../category.component';
+import { renderWithProvider } from '../../../utils/test/test.utils';
+
+jest.mock('react-router', () => ({
+    ...jest.requireActual('react-router'),
+    useParams: () => ({
+        category: 'mens'
+    }),
+}));
+
+describe('Category component Tests', () => {
+    test('It should render a spinner when isLoading is true', () => {
+        renderWithProvider(<Category />, {
+            preloadedState: {
+                categories: {
+                    isLoading: true,
+                    categories: [],
+                },
+            },
+        });
+
+        const spinner = screen.getByTestId('spinner');
+        expect(spinner).toBeInTheDocument();
+    });
+})
